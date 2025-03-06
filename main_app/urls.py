cc0ddc6ef465029e483bcf15cb935740371afd90
jimbojones1 <jimmyhaff3@gmail.com>
2025-03-06 11:49:45 -0500

    added day 1 solution

---
+++ main_app/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+# import all the functions in views file
+# as methods on a view object
+from . import views
+
+urlpatterns = [
+    # localhost:8000, 
+    # in the views folder we have home function
+    path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('cats/', views.cat_index, name='cat-index'),
+]
